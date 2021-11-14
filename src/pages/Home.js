d79098cfcd7dc90b1277adbcaf9b00fe637fe408
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-14 19:08:20 +0200

    Add homepage content.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,46 @@ import '../styles/homepage/Home.css';
 
 function Home() {
   return (
-    <section className="homepage">Home</section>
+    <section className="homepage">
+      <div className="banner">
+        <div className="name">
+          <p>Hi there!,</p>
+          <h1>
+            I&apos;m
+            {' '}
+            <font color="#ff3773">Simo</font>
+            {' '}
+            Nkosi
+          </h1>
+          <p className="details">
+            I’m a software developer!
+            I a strong passion for digital innovation. I can help you build interactive web
+            applications. Look through some of my work and experience!
+            If you like what you see and have a project you need coded,
+            don’t hesitate to contact me. I am very excited to work with you.
+          </p>
+          <a href="../assets/Simosakhe Dlamini Resume.pdf" target="_blank" className=" cv-btn"><div>Download Cv</div></a>
+        </div>
+        <div className="profile">
+          <img className=" profile-img" src="../assets/profile.jpg" alt="Simo Nkosi" />
+        </div>
+
+        <div className="effect-wrap">
+          <div className="effect effect-1" />
+          <div className="effect effect-2">
+            <div />
+          </div>
+          <div className="effect effect-3">
+            <div className="effect-3-0">
+              <div className="effect-3-1" />
+            </div>
+          </div>
+          <div className="effect effect-4" />
+          <div className="effect effect-4-0" />
+          <div className="effect effect-5" />
+        </div>
+      </div>
+    </section>
   );
 }
 
