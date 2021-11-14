9ce562e9b154bac19187b967b385c9fad564ef8b
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-14 16:52:53 +0200

    Add page Routes on app. - Update Navbar active className. - Update Navbar styling.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-// import { Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
 import './styles/main/App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,16 +6,16 @@ function Navbar() {
   return (
     <nav className="navbar">
       <div className="nav-list">
-        <NavLink to="/" className="nav-btn" activeclassname="active-navlink">
+        <NavLink to="/" className={(navData) => (navData.isActive ? 'active-navlink' : 'nav-btn')}>
           <i className="home icon active" />
         </NavLink>
-        <NavLink to="/about" className="nav-btn" activeclassname="active-navlink">
+        <NavLink to="/about" className={(navData) => (navData.isActive ? 'active-navlink' : 'nav-btn')}>
           <i className="info icon" />
         </NavLink>
-        <NavLink to="/projects" className="nav-btn" activeclassname="active-navlink">
+        <NavLink to="/projects" className={(navData) => (navData.isActive ? 'active-navlink' : 'nav-btn')}>
           <i className="tasks icon" />
         </NavLink>
-        <NavLink to="/contact" className="nav-btn" activeclassname="active-navlink">
+        <NavLink to="/contact" className={(navData) => (navData.isActive ? 'active-navlink' : 'nav-btn')}>
           <i className="tty icon" />
         </NavLink>
       </div>
