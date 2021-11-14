9d7b912f19938a65f8d530be4371f97de6a9d5b3
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-14 21:23:15 +0200

    Add homepage styling.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,26 +5,34 @@ function Home() {
   return (
     <section className="homepage">
       <div className="banner">
-        <div className="name">
-          <p>Hi there!,</p>
-          <h1>
-            I&apos;m
-            {' '}
-            <font color="#ff3773">Simo</font>
-            {' '}
-            Nkosi
-          </h1>
-          <p className="details">
-            I’m a software developer!
-            I a strong passion for digital innovation. I can help you build interactive web
-            applications. Look through some of my work and experience!
-            If you like what you see and have a project you need coded,
-            don’t hesitate to contact me. I am very excited to work with you.
-          </p>
-          <a href="../assets/Simosakhe Dlamini Resume.pdf" target="_blank" className=" cv-btn"><div>Download Cv</div></a>
+        <div className="banner-content">
+          <div className="introduction">
+            <p>Hi there!,</p>
+            <h1>
+              I&apos;m
+              {' '}
+              <font color="#ff3773">Simo</font>
+              {' '}
+              Nkosi
+            </h1>
+            <p className="details">
+              I’m a software developer!
+              I a strong passion for digital innovation. I can help you build interactive web
+              applications. Look through some of my work and experience!
+              If you like what you see and have a project you need coded,
+              don’t hesitate to contact me. I am very excited to work with you.
+            </p>
+          </div>
+          <div className="banner-actions">
+            <a href="../assets/Simosakhe Dlamini Resume.pdf" target="_blank">
+              <button type="button" className="cv-btn">Download Cv</button>
+            </a>
+          </div>
         </div>
         <div className="profile">
-          <img className=" profile-img" src="../assets/profile.jpg" alt="Simo Nkosi" />
+          <div className="profile-image">
+            <img className=" profile-img" src="../assets/profile.jpg" alt="Simo Nkosi" />
+          </div>
         </div>
 
         <div className="effect-wrap">
