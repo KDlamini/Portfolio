da1dd62ba1c41299baad239374d293388cf9cc3a
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-15 23:26:18 +0200

    Create Expertise section skeleton. - Add tab buttons with links to skills, experience, and education. - Update styling for all relevant components.

--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -1,17 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import '../styles/expertise/Expertise.css';
 
 function Expertise({ setIsOpen }) {
   return (
     <section className="expertise">
-      <button
-        type="button"
-        onClick={() => setIsOpen(true)}
-        className="go-back"
-      >
-        Go Back
-      </button>
+      <div className="about-tabs">
+
+        <button
+          type="button"
+          onClick={() => setIsOpen(true)}
+          className="go-back"
+        >
+          <i className="chevron left icon" />
+          Go Back
+        </button>
+
+        <Link to="/skills" className="tab-item">skills</Link>
+        <Link to="/experience" className="tab-item">education</Link>
+        <Link to="/education" className="tab-item">experience</Link>
+      </div>
+
       <div className="expertise-content">
         Expertise Section.
       </div>
