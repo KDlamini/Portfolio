7f7e374d5998ec57cc64f0be97f17442a9048b76
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-15 19:51:51 +0200

    Create Expertise component skeleton. - Render component on about page.

---
+++ src/components/Expertise.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/expertise/Expertise.css';
+
+function Expertise() {
+  return (
+    <section className="expertise">
+      <div className="expertise-content">
+        Expertise Section.
+      </div>
+    </section>
+  );
+}
+
+export default Expertise;
