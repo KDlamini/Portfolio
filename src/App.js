83b5fb529f8dcc432708bb201dcf36e92531c858
Simo Nkosi <wilzmar.kwd@gmail.com>
2021-11-16 11:10:16 +0200

    Move Expertise Routes to main app component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Expertise from './components/Expertise';
+import Skills from './components/Skills';
+import Experience from './components/Experience';
+import Education from './components/Education';
 import Home from './pages/Home';
 import About from './pages/About';
 import Projects from './pages/Projects';
@@ -19,7 +22,14 @@ function App() {
         {
           isOpen
             ? <Route path="/about" element={<About setIsOpen={setIsOpen} />} />
-            : <Route path="/expertise/*" element={<Expertise isOpen={isOpen} setIsOpen={setIsOpen} />} />
+            : (
+              <Route path="/expertise/*" element={<Expertise setIsOpen={setIsOpen} />}>
+                <Route path="" element={<Navigate to="skills" />} />
+                <Route path="skills" element={<Skills />} />
+                <Route path="experience" element={<Experience />} />
+                <Route path="education" element={<Education />} />
+              </Route>
+            )
         }
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
--- src/components/Expertise.js
+++ src/components/Expertise.js
@@ -1,23 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
-  Routes, Route, NavLink, useNavigate, Navigate,
+  Outlet, NavLink, useNavigate,
 } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Skills from './Skills';
-import Experience from './Experience';
-import Education from './Education';
 import '../styles/expertize/Expertise.css';
 
-function Expertise({ isOpen, setIsOpen }) {
+function Expertise({ setIsOpen }) {
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isOpen) {
-      navigate('/about');
-    }
-    // console.log(isOpen);
-  });
 
   return (
     <section className="expertise">
@@ -33,24 +23,18 @@ function Expertise({ isOpen, setIsOpen }) {
         </button>
 
         <NavLink to="skills" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>skills</NavLink>
-        <NavLink to="experience" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>education</NavLink>
-        <NavLink to="education" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>experience</NavLink>
+        <NavLink to="experience" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>experience</NavLink>
+        <NavLink to="education" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>education</NavLink>
       </div>
 
       <div className="expertise-content">
-        <Routes>
-          <Route path="/" element={<Navigate to="skills" />} />
-          <Route path="skills" element={<Skills />} />
-          <Route path="experience" element={<Experience />} />
-          <Route path="education" element={<Education />} />
-        </Routes>
+        <Outlet />
       </div>
     </section>
   );
 }
 
 Expertise.propTypes = {
-  isOpen: PropTypes.bool.isRequired,
   setIsOpen: PropTypes.func.isRequired,
 };
 
