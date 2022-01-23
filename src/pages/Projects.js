ef7d42cde761983d0a364a9461b92a94b7573344
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-23 16:26:57 +0200

    Add Routing for Web applications, Games, and E-commerce components.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -7,7 +7,7 @@ function Projects() {
     <section className="projects">
       <div className="navigation-tab">
         <NavLink to="all" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>All</NavLink>
-        <NavLink to="web applications" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>Web applications</NavLink>
+        <NavLink to="web-applications" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>Web applications</NavLink>
         <NavLink to="games" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>Games</NavLink>
         <NavLink to="e-commerce" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>E-commerce</NavLink>
       </div>
