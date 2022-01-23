b939e776210ee197907554797e6bd3ab2ab784f9
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-23 15:30:38 +0200

    Add projects basic structure.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Outlet, NavLink } from 'react-router-dom';
 import '../styles/projects/Projects.css';
 
 function Projects() {
   return (
-    <section className="projects">Projects</section>
+    <section className="projects">
+      <div className="project-tabs">
+        <NavLink to="skills" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>All</NavLink>
+        <NavLink to="experience" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>Web applications</NavLink>
+        <NavLink to="education" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>Games</NavLink>
+        <NavLink to="certification" className={(nav) => (nav.isActive ? 'buttonActive' : 'tab-item')}>E-commerce</NavLink>
+      </div>
+
+      <div className="project-content">
+        <Outlet />
+      </div>
+    </section>
   );
 }
 
