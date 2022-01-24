70b5317e76284b1f208c6274d7065e3a6e3b08db
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-01-25 00:01:57 +0200

    Update Certification page.

--- src/components/Certification.js
+++ src/components/Certification.js
@@ -3,10 +3,34 @@ import APIsMicroservices from '../assets/APIs-and-Microservices.png';
 import JavaScriptAlgorithmsAndDataStructures from '../assets/JavaScript-Algorithms-and-Data-Structures.png';
 import FrontEndLibraries from '../assets/Front-End-Libraries.png';
 import ResponsiveWebDesign from '../assets/Responsive-Web-Design.png';
+import ReactImg from '../assets/React.png';
+import JavaScriptImg from '../assets/JavaScript.png';
+import HtmlCssImg from '../assets/Html-Css.png';
 
 function Certification() {
   return (
     <section className="timeline">
+      <div className="certificate">
+        <h2>React and Redux</h2>
+        <div className="certificate-wrapper">
+          <img className="certificate-img" src={ReactImg} alt="React" />
+        </div>
+      </div>
+
+      <div className="certificate">
+        <h2>JavaScript</h2>
+        <div className="certificate-wrapper">
+          <img className="certificate-img" src={JavaScriptImg} alt="JavaScript" />
+        </div>
+      </div>
+
+      <div className="certificate">
+        <h2>Html and CSS</h2>
+        <div className="certificate-wrapper">
+          <img className="certificate-img" src={HtmlCssImg} alt="Html and Css" />
+        </div>
+      </div>
+
       <div className="certificate">
         <h2>APIs and Microservices</h2>
         <div className="certificate-wrapper">
